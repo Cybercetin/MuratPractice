2efbf414534f0d1ec20d0a6c14c5e10e2f56850f
Murat Cetin <mcetin@gmail.com>
2021-02-03 20:39:18 -0800

    Murat created add new user functionality.

--- src/main/java/MuratWork/Practice.java
+++ src/main/java/MuratWork/Practice.java
@@ -3,5 +3,8 @@ package MuratWork;
 public class Practice {
     public static void main(String[] args) {
 
+        // Started half of create new user functionality
+        // Finished creating new user functionality.
+
     }
 }
